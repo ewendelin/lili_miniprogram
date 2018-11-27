7df4eeffed203989bf1dd47a8fe35c2345fe442d
jiananwang <77843101@qq.com>
2018-11-27 17:56:48 +0800

    show page

---
+++ pages/show/show.js
@@ -0,0 +1,77 @@
+// pages/show/show.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    description: "The basis of Dutch apple pie is a crust on the bottom and around the edges.",
+    productname: "Apple pie",
+    discount:"40yuan",
+    oldprice:"30yuan",
+    latitude: 23.099994,
+    longitude: 113.324520,
+    starttime:"10:00",
+    endtime:"20:00",
+    everyday:"everyday",
+    restaurantname:"Grandpa's memory",
+    restaurantcuisine:"Dessert, Bakery & Pastries",
+    restaurantaddress:"Nanjing Xi Lu"
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
